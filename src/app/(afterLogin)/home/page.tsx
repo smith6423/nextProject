0fa37f2172aef0ea5da80ec97b18dd938183df0a
smith6423 <smith720@naver.com>
2024-01-01 21:51:29 +0900

    feat: Main page

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,17 @@
-export default function Home(){
-    return(
-        <>
-        home
-        </>
-    );
+import style from './home.module.css';
+import Tab from "@/app/(afterLogin)/home/_component/Tab";
+import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
+import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab/>
+        <PostForm />
+
+      </TabProvider>
+    </main>
+  )
 }
