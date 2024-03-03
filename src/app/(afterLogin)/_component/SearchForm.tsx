1eb1b77a99eb0c587ca2207734f7fbbdf15d3470
smith6423 <smith720@naver.com>
2024-03-03 21:33:46 +0900

    feat: explore page

--- src/app/(afterLogin)/_component/SearchForm.tsx
+++ src/app/(afterLogin)/_component/SearchForm.tsx
@@ -1,5 +1,6 @@
 import style from '@/app/(afterLogin)/_component/searchForm.module.css'
-export default function SearchForm (){
+type Props = { q?: string }
+export default function SearchForm({ q }: Props) {
     return(
         <form className={style.search}>
             <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,10 +1,25 @@
 import SearchForm from "../_component/SearchForm";
+import Trend from "../_component/Trend";
+import style from "./explore.module.css";
 
 export default function Explore(){
     return(
-        <main>
-            <div>
+        <main className={style.main}>
+            <div className={style.formZone}>
                 <SearchForm/>
+            </div>
+            <div className={style.trend}>
+                <h3>나를 위한 트렌드</h3>
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
             </div>
         </main>
     );
