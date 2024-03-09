703f33d4ddbae60a44c6d427c309aa6aa856ef2b
smith6423 <smith720@naver.com>
2024-03-09 22:19:03 +0900

    feat: Post, PostArticle

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -4,12 +4,14 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
 import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
-
+import PostArticle from './PostArticle';
+import {faker} from '@faker-js/faker'
 dayjs.locale('ko');
 dayjs.extend(relativeTime)
 
 export default function Post() {
   const target = {
+    postId:1,
     User: {
       id: 'elonmusk',
       nickname: 'Elon Musk',
@@ -17,10 +19,17 @@ export default function Post() {
     },
     content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
     createdAt: new Date(),
-    Images: [],
+    Images: [
+          ] as any[],
   }
+  if(Math.random()>0.5){
+    target.Images.push(
+      {imageId:1, link:faker.image.urlLoremFlickr()}
+    );  
+  }
+
   return (
-    <article className={style.post}>
+    <PostArticle post={target}>
       <div className={style.postWrapper}>
         <div className={style.postUserSection}>
           <Link href={`/${target.User.id}`} className={style.postUserImage}>
@@ -42,11 +51,14 @@ export default function Post() {
           </div>
           <div>{target.content}</div>
           <div className={style.postImageSection}>
+            {target.Images && target.Images.length>0 &&(
+                <div></div>
 
+            )}
           </div>
           <ActionButtons />
         </div>
       </div>
-    </article>
+    </PostArticle>
   )
 }
